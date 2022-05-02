f994b409084078ebd284f07a6efeb9665ae086b9
Carlos Sempe <sempejunior@gmail.com>
2022-05-02 20:02:29 -0300

    Tailwind example to use

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,7 +4,7 @@ interface ButtonProps{
 }
 
 function Button(props: ButtonProps) {
-  return <button className="bg-violet-500 p-2 h-10 rounded">{props.text}</button>
+  return <button className="bg-violet-500 p-2 h-10 rounded text-violet-100 hover:bg-violet-900">{props.text}</button>
 }
 
 
