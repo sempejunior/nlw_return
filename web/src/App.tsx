0283f079797a5edef01bb777ee3fe9d3c4969b94
Carlos Sempe <sempejunior@gmail.com>
2022-05-02 20:28:53 -0300

    Created widget component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,5 @@
+import { Widget } from "./components/Widget";
 
-interface ButtonProps{
-  text: string;
+export function  App(){
+  return <Widget />
 }
-
-function Button(props: ButtonProps) {
-  return <button className="bg-violet-500 p-2 h-10 rounded text-violet-100 hover:bg-violet-900">{props.text}</button>
-}
-
-
-function App() {
-  return (
-    <div className="flex gap-2">
-      <Button text="Enviar"/>
-      <Button text="Ok"/>
-
-
-    </div>
-
-
-  )
-}
-export default App
