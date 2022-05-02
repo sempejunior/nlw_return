2055d61d80a5573ee645030e0a3b7abf53b584a4
Carlos Sempe <sempejunior@gmail.com>
2022-05-02 20:37:12 -0300

    Created event onClick widget

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -1,10 +1,23 @@
 import { ChatTeardropDots } from 'phosphor-react';
+import { useState } from 'react';
 
 
 export function Widget() {
+
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+    function toogleWidgetVisibility(){
+        setIsWidgetOpen(!isWidgetOpen);
+    }
+
     return (
         <div className='absolute bottom-5 right-5'>
-            <button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+            
+            {/* Só executa o hello world se a variavel isWidgetOpen for true */}
+            {isWidgetOpen && <p>Hello World</p>}
+            
+            
+            <button onClick={toogleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className="w-6 h-6" />
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
                     <span className='pl-2'></span>
